a6e84b45c50d7fff8b477ffc646654987c108bb7
dragonstreak_1 <dragonstreak_1@163.com>
2013-12-24 01:01:23 +0800

    AddArticle Test Case

--- RuiJi.Core/RuiJiDataAccess.FunctionTest/ArticleTest.cs
+++ RuiJi.Core/RuiJiDataAccess.FunctionTest/ArticleTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuiJi.DataAccess;
+using RuiJi.DataAccess.Articles;
+using RuiJi.DataAccess.Models;
 
 namespace RuiJiDataAccess.FunctionTest
 {
@@ -9,6 +12,25 @@ namespace RuiJiDataAccess.FunctionTest
         [TestMethod]
         public void AddTest()
         {
+            var svc = GetService();
+            Article article = new Article();
+
+            article.ArticleTypeId = 1;
+            article.Author = "Tony";
+            article.ContentDetail = "hello world";
+            article.IsPublished = true;
+            article.IsDeleted = false;
+            article.PublishDate = new DateTime(2013, 12, 23);
+            article.Title = "First Article";
+            article.InsertBy = "Tony";
+            article.UpdateBy = "Tony";
+
+            svc.Add(article);
+        }
+
+        private IArticleSvc GetService()
+        {
+            return RuiJiPortalServiceLocator.Instance.GetSvc<IArticleSvc>();
         }
     }
 }
