12b197f33ed8b36f1417cc491b27ffdbf5f450ce
Astranagant <Astranagant>
2013-12-30 10:04:03 +0800

    merge base

---
+++ RuiJi.UI/RuiJi.Web/MvcBase/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Mvc;
+
+namespace RuiJi.Web.MvcBase
+{
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ RuiJi.UI/RuiJi.Web/MvcBase/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RuiJi.Web.MvcBase
+{
+    public class BaseModel
+    {
+        public bool IsSuccess { get; set; }
+
+        public string ErrorMsg { get; set; }
+    }
+}
