eb7a11e394b128779c90c929952a0b6327f377d1
dragonstreak_1 <dragonstreak_1@163.com>
2014-01-07 10:48:57 +0800

    add cache and UI style

---
+++ RuiJi.Internal/RuiJi.Internal/Models/BaseModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RuiJi.Internal.Models
+{
+    public class BaseModel
+    {
+        public string CurrentMenu { get; set; }
+
+        public string CurrentSubMenu { get; set; }
+
+        private int pageScroll = 0;
+        public int PageScroll
+        {
+            get
+            {
+                return pageScroll;
+            }
+            set
+            {
+                pageScroll = value;
+            }
+        }
+
+        private string pageMessage = string.Empty;
+        public string PageMessage
+        {
+            get { return pageMessage; }
+            set { pageMessage = value; }
+        }
+
+        public void SetMenu(string menu, string subMenu)
+        {
+            this.CurrentMenu = menu;
+            this.CurrentSubMenu = subMenu;
+        }
+    }
+}
