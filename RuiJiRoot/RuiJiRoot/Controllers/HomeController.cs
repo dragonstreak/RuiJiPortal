55a1ce354ac54a2073d089d2df99348a9d3a9011
dragonstreak <dragonstreak_1@163.com>
2014-03-08 22:29:37 +0800

    add ruiji root site

---
+++ RuiJiRoot/RuiJiRoot/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RuiJiRoot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+           return Redirect("~/RuiJiPortal");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
